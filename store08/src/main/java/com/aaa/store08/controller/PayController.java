bfb0f5f7fa564a00a16f3c521ad010b364b5aca4
git-leixi <194153518@qq.com>
2019-11-08 10:50:22 +0800

    修改了路由   赵会祥

--- store08/src/main/java/com/aaa/store08/controller/PayController.java
+++ store08/src/main/java/com/aaa/store08/controller/PayController.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/member")
 public class PayController {
@@ -59,6 +57,6 @@ public class PayController {
             System.out.println("空");
         }
 
-        return "redirect:../login/hello";
+        return "page/index";
     }
 }
