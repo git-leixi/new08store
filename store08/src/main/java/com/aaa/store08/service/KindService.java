8dfd32fccd33984d8e8f5eea3136bef49f364928
git-leixi <1437683386@qq.com>
2019-11-18 18:13:23 +0800

    lmc   菜品种类  查询  删除

--- store08/src/main/java/com/aaa/store08/service/KindService.java
+++ store08/src/main/java/com/aaa/store08/service/KindService.java
@@ -3,8 +3,15 @@ package com.aaa.store08.service;
 import com.aaa.store08.entity.Kind;
 
 import java.util.List;
+import java.util.Map;
 
 public interface KindService {
+    //查询餐饮种类
     public List<Kind> SelKind();
-    public int InsertKind(Kind kind);
+    //添加餐饮类别
+    public int InsertKind(String kname);
+    //删除 餐饮类别
+    int delKind(int kid);
+    //修改  餐饮类别名称
+    int updKind(Map map);
 }
